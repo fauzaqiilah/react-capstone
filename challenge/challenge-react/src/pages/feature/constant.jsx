130cdb7562cf2bb6554dc3d439d18ba9e13d1b6c
fauzaqiilah <sqiqilah@gmail.com>
2023-04-12 00:34:38 +0700

    update1

---
+++ challenge/challenge-react/src/pages/feature/constant.jsx
@@ -0,0 +1,67 @@
+export const JOIN_DATA = [
+    {
+        id: "1",
+        image: "https://www.alterra.id/wp-content/uploads/2020/02/icon1@2x.png",
+        teks: "Aggregator resmi dari berbagai telco provider populer di Indonesia.",
+    },
+    
+]
+
+export const PRODUCT_DATA = [
+    {
+        id: "1",
+        image2: "https://www.alterra.id/wp-content/uploads/2020/01/alterra-bills-01.png",
+        judul2: "Alterra Bills",
+        teks2: "Layanan yang dapat menghubungkan pemilik tagihan (Bill Issuer), dengan berbagai kanal pembayaran, digital commerce, dan juga end-users.",
+    },
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const USERS_DATA1 = [
+//     {
+//         id: "1",
+//         name: "Alterra Bills",
+//         img: Alterrabils,
+//         tekscard: "Layanan yang dapat menghubungkan pemilik tagihan (Bill Issuer), dengan berbagai kanal pembayaran, digital commerce, dan juga end-users.",
+//     },
+//     {
+//         id: "2",
+//         name: "Alterra Academy",
+//         img: Alterralogo,
+//         tekscard: "Tech talent incubator yang memberikan kesempatan bagi banyak individu untuk menjadi tech talent profesional dan berkualitas bahkan tanpa latar Pendidikan IT.",
+//     },
+//     {
+//         id: "3",
+//         name: "Alterra Pay",
+//         img: Alterrapay,
+//         tekscard: "Sistem pembayaran online dan rekonsiliasi otomatis yang terintegrasi untuk membantu bisnis Anda berjalan lebih efektif dan efisien.",
+//     },
+//     {
+//         id: "4",
+//         name: "Bimasakti",
+//         img: Bimasakti,
+//         tekscard: "Anak perusahaan Alterra yang melayani kebutuhan bisnis & organisasi seperti PDAM, pemerintah daerah, rumah sakit, dsb, melalui penyediaan teknologi yang dapat menunjang aktivitas operasional.",
+//     },
+// ]
+
+// export { USERS_DATA, USERS_DATA1 };
--- challenge/challenge-react/src/pages/feature/reactCaptone.jsx
+++ challenge/challenge-react/src/pages/feature/reactCaptone.jsx
@@ -1,118 +1,158 @@
 import React from "react";
-import { Card, Col, Row } from "antd";
-import "./reactCaptone.css";
+import { JOIN_DATA } from "./constant";
+import { PRODUCT_DATA } from "./constant";
 import { ArrowRightOutlined } from "@ant-design/icons";
+import { Col, Row, Card } from "antd";
+import "./reactCaptone.css";
+import Gap from "../../component/gap/Gap";
 
 const ReactCaptone = () => {
   return (
     <>
-      <h3>Why Join Us?</h3>
-      <Row className="card">
-        <Col span={6} className="konten">
-          <img src="https://www.alterra.id/wp-content/uploads/2020/02/icon1@2x.png" />
-          <p>
-            Aggregator resmi dari berbagai telco provider populer di Indonesia.
-          </p>
-        </Col>
-        <Col span={6} className="konten">
-          <img src="https://www.alterra.id/wp-content/uploads/2020/02/icon2@2x.png" />
-          <p>
-            Tersedia berbagai pilihan layanan tagihan dan pembayaran (bill
-            payment) yang sesuai kebutuhan bisnis Anda.
-          </p>
-        </Col>
-        <Col span={6} className="konten">
-          <img src="https://www.alterra.id/wp-content/uploads/2020/02/icon3@2x.png" />
-          <p>Mampu memproses puluhan juta transaksi setiap harinya.</p>
-        </Col>
-        <Col span={6} className="konten">
-          <img src="https://www.alterra.id/wp-content/uploads/2020/02/icon4@2x.png" />
-          <p>
-            Telah bermitra dengan lebih dari 200 online partners dan jumlahnya
-            terus bertambah.
-          </p>
-        </Col>
-      </Row>
-      <h3>Our Products and Services</h3>
-      <Row className="card2" gutter={25}>
-        <Col span={6}>
-          <Card>
-            <img
-              src="https://www.alterra.id/wp-content/uploads/2020/01/alterra-bills-01.png"
-              width="77"
-            />
-            <h5>Alterra Bills</h5>
-            <p>
-              Layanan yang dapat menghubungkan pemilik tagihan (Bill Issuer),
-              dengan berbagai kanal pembayaran, digital commerce, dan juga
-              end-users.
-            </p>
-            {
-              <a href="https://alterrabills.id/" target="_blank">
-                Learn More <ArrowRightOutlined />
-              </a>
-            }
+      <h4>Why Join Us?</h4>
+
+      {JOIN_DATA.map((join, index) => (
+        <Row key={index} className="row">
+          <Card className="card">
+            <Col className="gutter-row">
+              <img src={join.image} alt="" className="foto" />
+              <Gap height={10} />
+              <div className="konten">{join.teks}</div>
+            </Col>
           </Card>
-        </Col>
-        <Col span={6}>
-          <Card>
-            <img
-              src="https://www.alterra.id/wp-content/uploads/2020/02/academy.png"
-              width="77"
-            />
-            <h5>Alterra Academy</h5>
-            <p>
-              Tech talent incubator yang memberikan kesempatan bagi banyak
-              individu untuk menjadi tech talent profesional dan berkualitas
-              bahkan tanpa latar Pendidikan IT.
-            </p>
-            {
-              <a href="https://academy.alterra.id/" target="_blank">
-                Learn More <ArrowRightOutlined />
-              </a>
-            }
+
+          <Card className="card">
+            <Col className="gutter-row">
+              <img src={join.image} alt="" className="foto" />
+              <Gap height={10} />
+              <div className="konten">{join.teks}</div>
+            </Col>
           </Card>
-        </Col>
-        <Col span={6}>
-          <Card>
-            <img
-              src="https://www.alterra.id/wp-content/uploads/2020/01/logo_alterra_pay@2x.png"
-              width="77"
-            />
-            <h5>Alterra Pay</h5>
-            <p>
-              Sistem pembayaran online dan rekonsiliasi otomatis yang
-              terintegrasi untuk membantu bisnis Anda berjalan lebih efektif dan
-              efisien.
-            </p>
-            {
-              <a href="https://alterrapay.id/" target="_blank">
-                Learn More <ArrowRightOutlined />
-              </a>
-            }
+
+          <Card className="card">
+            <Col className="gutter-row">
+              <img src={join.image} alt="" className="foto" />
+              <Gap height={10} />
+              <div className="konten">{join.teks}</div>
+            </Col>
           </Card>
-        </Col>
-        <Col span={6}>
-          <Card>
-            <img
-              src="https://www.alterra.id/wp-content/uploads/2020/01/logo_bimasakti@2x.png"
-              width="77"
-            />
-            <h5>Bimasakti</h5>
-            <p>
-              Anak perusahaan Alterra yang melayani kebutuhan bisnis &
-              organisasi seperti PDAM, pemerintah daerah, rumah sakit, dsb,
-              melalui penyediaan teknologi yang dapat menunjang aktivitas
-              operasional.
-            </p>
-            {
-              <a href="https://bsa.id/" target="_blank">
-                Learn More <ArrowRightOutlined />
-              </a>
-            }
+
+          <Card className="card">
+            <Col className="gutter-row">
+              <img src={join.image} alt="" className="foto" />
+              <Gap height={10} />
+              <div className="konten">{join.teks}</div>
+            </Col>
           </Card>
-        </Col>
-      </Row>
+        </Row>
+      ))}
+
+      <h4>Our Products and Services</h4>
+
+      {PRODUCT_DATA.map((product, index) => (
+        <Row key={index} className="row2">
+          <Card
+            className="card2"
+            hoverable
+            style={{
+              marginRight: "15px",
+              color: "#19345E",
+            }}
+          >
+            <Col className="gutter-row">
+              <img src={product.image2} alt="" className="foto2" />
+              <Gap height={10} />
+              <div className="judul">{product.judul2}</div>
+              <Gap height={10} />
+              <div className="konten2">{product.teks2}</div>
+              <a
+                    href="https://alterrabills.id/"
+                    className="read-more"
+                    target="_blank"
+                  >
+                    <b>Learn More</b><ArrowRightOutlined /></a>
+            </Col>
+          </Card>
+
+          <Gap width={20} />
+
+          <Card
+            className="card2"
+            hoverable
+            style={{
+              marginRight: "15px",
+              color: "#19345E",
+            }}
+          >
+            <Col className="gutter-row">
+              <img src={product.image2} alt="" className="foto2" />
+              <Gap height={10} />
+              <div className="judul">{product.judul2}</div>
+              <Gap height={10} />
+              <div className="konten2">{product.teks2}</div>
+              <a
+                    href="https://alterrabills.id/"
+                    className="read-more"
+                    target="_blank"
+                  >
+                    <b>Learn More</b><ArrowRightOutlined />
+                  </a>
+            </Col>
+          </Card>
+
+          <Gap width={20} />
+
+          <Card
+            className="card2"
+            hoverable
+            style={{
+              marginRight: "15px",
+              color: "#19345E",
+            }}
+          >
+            <Col className="gutter-row">
+              <img src={product.image2} alt="" className="foto2" />
+              <Gap height={10} />
+              <div className="judul">{product.judul2}</div>
+              <Gap height={10} />
+              <div className="konten2">{product.teks2}</div>
+              <a
+                    href="https://alterrabills.id/"
+                    className="read-more"
+                    target="_blank"
+                  >
+                    <b>Learn More</b><ArrowRightOutlined />
+                  </a>
+            </Col>
+          </Card>
+
+          <Gap width={20} />
+
+          <Card
+            className="card2"
+            hoverable
+            style={{
+              marginRight: "15px",
+              color: "#19345E",
+            }}
+          >
+            <Col className="gutter-row">
+              <img src={product.image2} alt="" className="foto2" />
+              <Gap height={10} />
+              <div className="judul">{product.judul2}</div>
+              <Gap height={10} />
+              <div className="konten2">{product.teks2}</div>
+              <a
+                    href="https://alterrabills.id/"
+                    className="read-more"
+                    target="_blank"
+                  >
+                    <b>Learn More</b><ArrowRightOutlined />
+                  </a>
+            </Col>
+          </Card>
+        </Row>
+      ))}
     </>
   );
 };
